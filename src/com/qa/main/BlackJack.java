4c01e537c1321d8466331f60d1ebc79f3db87851
Korbinian <kmr.ring@outlook.com>
2020-03-24 17:02:57 +0000

    first commit

---
+++ src/com/qa/main/BlackJack.java
@@ -0,0 +1,24 @@
+package com.qa.main;
+
+public class BlackJack {
+
+	public int play(int Number1, int Number2) {
+		if (Number1 > 21 && Number2 > 21) {
+			return 0;
+		}
+		else if (Number2 > 21) {
+			return Number1;
+		}
+		else if (Number1 > 21) {
+			return Number2;
+		}
+		else if (Number1 > Number2) {
+			return Number1;
+		}
+		else {
+			return Number2;
+		}
+
+	}
+
+}
