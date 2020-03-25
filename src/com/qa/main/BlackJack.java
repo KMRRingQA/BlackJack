a8cafca8818b827f5ec76d27bfe437c3e1bc8993
Korbinian <kmr.ring@outlook.com>
2020-03-25 09:44:09 +0000

    made code more efficient

--- src/com/qa/main/BlackJack.java
+++ src/com/qa/main/BlackJack.java
@@ -9,9 +9,6 @@ public class BlackJack {
 		else if (Number2 > 21) {
 			return Number1;
 		}
-		else if (Number1 > 21) {
-			return Number2;
-		}
 		else if (Number1 > Number2) {
 			return Number1;
 		}
