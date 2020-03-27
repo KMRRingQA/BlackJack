ff4ab5aec18f0e933290db84cf47b6fc6e1ff556
Korbinian <kmr.ring@outlook.com>
2020-03-27 18:10:24 +0000

    naming etiquette

--- src/com/qa/main/BlackJack.java
+++ src/com/qa/main/BlackJack.java
@@ -2,21 +2,21 @@ package com.qa.main;
 
 public class BlackJack {
 
-	public int play(int Number1, int Number2) {
-		if (Number1 > 21 && Number2 > 21) {
+	public int play(int number1, int number2) {
+		if (number1 > 21 && number2 > 21) {
 			return 0;
 		}
-		else if (Number2 > 21) {
-			return Number1;
+		else if (number2 > 21) {
+			return number1;
 		}
-		else if (Number1 > 21) {
-			return Number2;
+		else if (number1 > 21) {
+			return number2;
 		}
-		else if (Number1 > Number2) {
-			return Number1;
+		else if (number1 > number2) {
+			return number1;
 		}
 		else {
-			return Number2;
+			return number2;
 		}
 
 	}
